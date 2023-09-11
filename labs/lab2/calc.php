1fce948feef41700afcfe549f4f4587bee8f137a
basma2h <104505695+basma2h@users.noreply.github.com>
2023-09-11 15:41:05 +0300

    Create calc.php

---
+++ labs/lab2/calc.php
@@ -0,0 +1,21 @@
+<?php
+        echo "<h1> Basic of Calculation</h1>";
+            
+  function add($x,$y){
+    return ($x+$y);
+}
+        function subtract($x,$y){
+    return ($x-$y);
+}         
+        function mutiply($x,$y){
+    return ($x*$y);
+}        
+        function devide($x,$y){
+    return ($x/$y);
+}    
+       echo "2+5= ".add(2,5)."<br>";
+      echo  "7-4= ".subtract(7,4)."<br>";
+        echo "3*5= ".mutiply(3,5)."<br>";
+       echo "8/4= ".devide(8,4)."<br>";
+     
+?>
